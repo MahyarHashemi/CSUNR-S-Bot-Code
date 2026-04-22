b56453541d5a4428f75238a0a13ba275d1a0f6f4
MahyarHashemi <88331830+MahyarHashemi@users.noreply.github.com>
2026-04-22 12:12:27 -0700

    Added Autos to Main

--- src/main.cpp
+++ src/main.cpp
@@ -60,6 +60,10 @@ void initialize() {
 
   // Autonomous Selector using LLEMU
   ez::as::auton_selector.autons_add({
+      {"Slow Score of Long Goal and Match Loads, Then Hook In", laz_long_descore_slow_match},
+      {"Quick Score of Long Goal, Then Hook In", laz_long_descore_quick_match},
+      {"Mid then Long Goal", laz_mid_long_match_auton},
+      {"Long Then Middle Goal", laz_long_mid_match_auton},
       {"Randomly select one of the four match autos", random_auton},
       {"Slow Score of Long Goal and Match Loads, Then Hook In", long_descore_slow_match},
       {"Quick Score of Long Goal, Then Hook In", long_descore_quick_match},
